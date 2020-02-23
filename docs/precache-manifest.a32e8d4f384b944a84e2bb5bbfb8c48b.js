d6a7bbe442d1ce023e183bffb870bbc22251473c
Fanaticys <dmkndrtnk@gmail.com>
2020-02-23 15:43:16 +0200

    add docs folder

---
+++ docs/precache-manifest.a32e8d4f384b944a84e2bb5bbfb8c48b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd28f32343fa90e8eaed218e3d16ad0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b92aeb13cec1b848db8",
+    "url": "/static/css/main.c0c92423.chunk.css"
+  },
+  {
+    "revision": "aa5423bd25d31a064a1c",
+    "url": "/static/js/2.609c6d73.chunk.js"
+  },
+  {
+    "revision": "ef838b08f082d354fbd3f14a41ff4921",
+    "url": "/static/js/2.609c6d73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b92aeb13cec1b848db8",
+    "url": "/static/js/main.61c8180f.chunk.js"
+  },
+  {
+    "revision": "bd0e0c3bb824f4e11e48",
+    "url": "/static/js/runtime-main.32dee1c0.js"
+  }
+]);
